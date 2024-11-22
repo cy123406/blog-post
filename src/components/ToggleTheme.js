33ba2f0872f07c0096cd34f7b276fc6b3faddebf
cheney <601696@gree.com>
2024-11-22 13:56:19 +0800

    pref: 将主题加入localstroage保持页面一致性

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,20 +1,28 @@
 import { createTheme, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
 
+const THEME = "theme";
+
 export const GetTheme = () => {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
     const [themeMode, setThemeMode] = useState(prefersDarkMode ? 'dark' : 'light')
     useEffect(() => {
-        setThemeMode(prefersDarkMode ? 'dark' : 'light');
-    }, [prefersDarkMode]);
+        const prefersThemeMode = window.localStorage.getItem(THEME);
+        if (prefersThemeMode === null) {
+            setThemeMode(prefersDarkMode ? 'dark' : 'light');
+        }
+        else setThemeMode(prefersThemeMode)
+    }, []);
     const toggleTheme = () => {
-        setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        const newTheme = themeMode === 'light' ? 'dark' : 'light';
+        setThemeMode(newTheme);
+        window.localStorage.setItem(THEME, newTheme);
     };
     const theme = createTheme({
         palette: {
             mode: themeMode,
         },
-        customMethods:{
+        customMethods: {
             toggleTheme,
         }
     });
